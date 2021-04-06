2ee850f5ee14edc29aaf0a0aaa1cd6803540a8db
April.Chen <forchpeng@163.com>
2021-04-06 20:12:19 +0800

    Initial commit

---
+++ src/main/java/com/chanpion/graphql/GraphApplication.java
@@ -0,0 +1,16 @@
+package com.chanpion.graphql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author April Chen
+ * @date 2021/2/18 16:02
+ */
+@SpringBootApplication
+public class GraphApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GraphApplication.class, args);
+    }
+}
---
+++ src/main/java/com/chanpion/graphql/service/Mutation.java
@@ -0,0 +1,10 @@
+package com.chanpion.graphql.service;
+
+import com.coxautodev.graphql.tools.GraphQLMutationResolver;
+
+/**
+ * @author April Chen
+ * @date 2021/2/18 16:29
+ */
+public class Mutation  implements GraphQLMutationResolver {
+}
